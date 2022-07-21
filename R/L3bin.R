9f473b3bc03e07dfd1d9d03b2c6c87576fd3ba72
Michael Sumner <mdsumner@gmail.com>
2022-07-21 12:57:18 +1000

    bit more features, readme

--- R/L3bin.R
+++ R/L3bin.R
@@ -20,9 +20,11 @@
 #' crop_bins(init, c(100, 110, -50, -45))
 #' @export
 crop_bins <- function(x, extent) {
+  if (any(diff(extent)[c(1, 3)] <= 0)) stop("invalid extent, must be c(xmin, xmax, ymin, ymax)")
   ext <- extent
   nrows <- length(x$basebin)
   ilat <- which(x$latbin >= ext[3L] & x$latbin <= ext[4L] )
+
   ilat <- .snapout1(ilat, 1L, nrows)
 
   basebin <- x$basebin[ilat]
@@ -75,6 +77,25 @@ bin_from_lonlat <- function(lon, lat, NUMROWS) {
   as.integer(ibin$basebin[row] + col)
 }
 
+#' Longitude and latitude from bin number.
+#'
+#' Generate longitude and latitude coordinates from bin number.
+#'
+#' Originally from sosoc/croc where it is called bin2lonlat
+#' @param bins bin number
+#' @param nrows number of rows in this grid
+#' @export
+lonlat_from_bin <- function(bins, nrows) {
+  row <- seq_len(nrows) - 1
+  latbin = ((row + 0.5)*180.0/nrows) - 90.0;
+  numbin <- as.integer((2*nrows*cos(latbin*pi/180.0) + 0.5))
+  basebin <- c(1L, utils::head(cumsum(numbin) + 1L, -1L))
+  totbins <- utils::tail(basebin, 1) + utils::tail(numbin, 1) - 1
+  index <- findInterval(bins, basebin)
+  lat <- latbin[index]
+  lon <- 360.0*(bins - basebin[index] + 0.5)/numbin[index] - 180.0;
+  cbind(lon, lat)
+}
 
 ' Initialize values for a particular binning
 #'
@@ -95,4 +116,29 @@ L3bin <- function(NUMROWS = 2160) {
   basebin <- cumsum(c(1L, numbin[-length(numbin)]))
   totbins = basebin[NUMROWS] + numbin[NUMROWS] - 1
   list(latbin = latbin, numbin = numbin, basebin = basebin, totbins = totbins)
+}
+
+
+#' Calculate bin boundaries from bin number
+#'
+#' Calculate bin boundaries from bin number
+#'
+#' Originally from sosoc/croc where it is called bin2bounds
+#' @param bin bin number
+#' @param NUMROWS relevant number of L3 bin rows
+#' @export
+extent_from_bin <- function(bin, NUMROWS) {
+  row = NUMROWS - 1;
+  latbin <- (((seq(NUMROWS) - 1) + 0.5) * 180 / NUMROWS ) - 90
+  numbin <- as.integer(2 * NUMROWS * cos(latbin * pi/180) + 0.5)
+  basebin <- cumsum(c(1L, numbin[-length(numbin)]))
+  fint <- findInterval(bin, basebin)
+  north <- latbin[fint] + 90.0/NUMROWS
+  south <- latbin[fint] - 90.0/NUMROWS
+  ##*north = latbin[row] + 90.0/NUMROWS;
+  ##*south = latbin[row] - 90.0/NUMROWS;
+  lon = 360.0*(bin - basebin[fint] + 0.5)/numbin[fint] - 180.0;
+  west = lon - 180.0/numbin[fint];
+  east = lon + 180.0/numbin[fint];
+  cbind(xmin = west, xmax =   east, ymin = south,  ymax = north)
 }
---
+++ README.md
@@ -0,0 +1,90 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# L3bin
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of L3bin is to provide the [Integerized Sinusoidal Binning
+Scheme](https://oceancolor.gsfc.nasa.gov/docs/format/l3bins/) used by
+NASA’s Ocean Biology processing Group for Level 3 data.
+
+## Installation
+
+You can install the development version of L3bin from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("hypertidy/L3bin")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(L3bin)
+## basic example code
+nr <- 6
+bins <- L3bin(nr)
+ex <- extent_from_bin(1:bins$totbins, nr)
+
+plot(range(ex[,1:2]), range(ex[,3:4]))
+rect(ex[,1], ex[,3], ex[,2], ex[, 4])
+```
+
+<img src="man/figures/README-example-1.png" width="100%" />
+
+``` r
+
+
+nr <- 12
+bins <- L3bin(nr)
+lbins <- crop_bins(bins, extent = c(-20, 30, -60, -10))
+ex <- extent_from_bin(lbins, nr)
+
+plot(range(ex[,1:2]), range(ex[,3:4]))
+rect(ex[,1], ex[,3], ex[,2], ex[, 4])
+```
+
+<img src="man/figures/README-example-2.png" width="100%" />
+
+``` r
+
+ex <- extent_from_bin(1:bins$totbins, nr)
+
+plot(range(ex[,1:2]), range(ex[,3:4]))
+rect(ex[,1], ex[,3], ex[,2], ex[, 4])
+```
+
+<img src="man/figures/README-example-3.png" width="100%" />
+
+Get lonlat from bin and vice versa.
+
+``` r
+plot(range(ex[,1:2]), range(ex[,3:4]))
+rect(ex[,1], ex[,3], ex[,2], ex[, 4])
+
+text(lonlat_from_bin(1:bins$totbins, nr), lab = 1:bins$totbins)
+abline(v = 147, h = -42, col = "firebrick")
+```
+
+<img src="man/figures/README-bin-1.png" width="100%" />
+
+``` r
+
+bin_from_lonlat(147, -42, nr)
+#> [1] 45
+```
+
+No reprojecting yet, need to check some stuff and probably densify the
+edges … there is densify funs in sosoc/croc WIP
+
+## Code of Conduct
+
+Please note that the L3bin project is released with a [Contributor Code
+of
+Conduct](https://contributor-covenant.org/version/2/1/CODE_OF_CONDUCT.html).
+By contributing to this project, you agree to abide by its terms.
